2de2f50c7e831de907f0bafc03b82a90c166b1e3
Pukar Chhatkuli <rockypukar@pop-os.localdomain>
2023-09-18 16:14:45 +0545

    first commit

---
+++ src/pages/DeleteBook.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import axios from "axios";
+
+const DeleteBook = () => {
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const handleDeleteBook = () => {
+    setLoading(true);
+    axios
+      .delete(`http://localhost:5555/books/${id}`)
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert("An error occured. Please check console");
+        console.log(error);
+      });
+  };
+  return (
+    <div className="p-4">
+      <BackButton />
+      <div className="text-2xl my-4">Delete Book</div>
+      {loading && <Spinner />}
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
+        <div className="text-2xl">
+          Are you sure you want to delete this book. This cannot be undone
+        </div>
+        <button
+          className="p-4 bg-red-600 text-white m-8 w-full"
+          onClick={handleDeleteBook}
+        >
+          Yes, Delete it
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteBook;
